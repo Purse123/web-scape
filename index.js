9a8c2b56696b89079e66f230f54de76d39ba007f
Purse123 <sahasneupane85@gmail.com>
2024-07-05 17:30:19 +0545

    book for scrape

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const puppeteer = require("puppeteer");
 const fs = require('fs');
 // const url = "https://www.instagram.com/p/C5lecH8NqJS/";
-// const url = "https://www.instagram.com/bakchod_gang1/";
-const url = "https://books.toscrape.com/";
+const url = "https://www.instagram.com/bakchod_gang1/";
+// const url = "https://books.toscrape.com/";
 
 const main = async () => {
   const browser = await puppeteer.launch({ headless: false });
@@ -10,15 +10,15 @@ const main = async () => {
   await page.goto(
     url,
     /* until page loads */
-    // { waitUntil: "networkidle2" }
+    { waitUntil: "networkidle2" }
   );
   // evaluate() takes all the code from the screen and create a document
   const Scrapper = await page.evaluate((url) => {
     const posts = Array.from(
-    //   document.querySelectorAll(
-    //     ".x1lliihq .x1n2onr6 .xh8yej3 .x4gyw5p .xfllauq .xo2y696 .x11i5rnm .x2pgyrj"
-    //   )
-      document.querySelectorAll(".product_pod")
+      document.querySelectorAll(
+        ".x1lliihq .x1n2onr6 .xh8yej3 .x4gyw5p .xfllauq .xo2y696 .x11i5rnm .x2pgyrj"
+      )
+      // document.querySelectorAll(".product_pod")
     );
     const postData = posts.map((post) => ({
         title: post.querySelector('h3 a').getAttribute('title'),
